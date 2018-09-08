da41287b10b66259f13ba7bfc635868bbb117a26
Kaan HAS <kaanhas.52@gmail.com>
2018-09-09 00:21:41 +0300

    #5 Added web interface for user use and testing.

---
+++ RomanNumerals.Web/Scripts/pageViewJS/romanNumerals.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+    romanNumInit();
+});
+
+
+var romanNumInit = function () {
+    $('#btnRomanParse').on('click', function () {
+        var number = $('#txtNumber').val();
+
+        $('#lblRomanResult').val('');
+        romanNumConvert(number);
+    });
+};
+
+var romanNumConvert = function (number) {
+
+    $.ajax({
+        url: 'http://localhost:30667/api/RomanNumerals/Converter',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            number: number
+        },
+        beforeSend: function (xhr) {
+            $('#lblRomanResult').text('');
+        },
+        success: function (data, textStatus, xhr) {
+            if (data.IsSuccessful) {
+                $('#lblRomanResult').text(data.Data);
+            } else {
+                alert(data.Message);
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert(textStatus + '\n\n' + xhr.responseText);
+        }
+    });
+};
