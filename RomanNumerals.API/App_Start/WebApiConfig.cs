a80b8d9bc3f3eefd808ce1f628f4e2108c1094f7
Kaan HAS <kaanhas.52@gmail.com>
2018-09-09 00:01:38 +0300

    #3 The API project was developed and a service was added to translate the roman numerals.

--- RomanNumerals.API/App_Start/WebApiConfig.cs
+++ RomanNumerals.API/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using RomanNumerals.API.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace RomanNumerals.API
 {
@@ -11,12 +13,22 @@ namespace RomanNumerals.API
         {
             // Web API configuration and services
 
+            // XML Response delete, only JSON Response
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            // Added model state filters.
+            config.Filters.Add(new ValidateModelAttribute());
+            config.Filters.Add(new CheckModelForNullAttribute());
+
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ RomanNumerals.API/Controllers/RomanNumeralsController.cs
@@ -0,0 +1,49 @@
+﻿using RomanNumerals.Core.Model;
+using RomanNumerals.Core.Operation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace RomanNumerals.API.Controllers
+{
+    public class RomanNumeralsController : ApiController
+    {
+        #region [Fields]
+
+        private Core.Operation.RomanNumerals roman;
+
+        #endregion
+
+
+        #region [Constructors]
+
+        public RomanNumeralsController()
+        {
+            roman = new Core.Operation.RomanNumerals();
+        }
+
+        #endregion
+
+
+        [HttpGet]
+        public Result<string> Converter(int number)
+        {
+            var result = new Result<string>();
+            try
+            {
+                result.Data = roman.Parse(number);
+                result.IsSuccessful = true;
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccessful = false;
+                result.Message = ex.Message;
+            }
+
+
+            return result;
+        }
+    }
+}
